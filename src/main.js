1cf441a2109da85ab98b3d864e2ccd0587670675
Robins <robinmgk077@gmail.com>
2024-01-19 20:56:51 +0300

    latest commit

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
+// Function to handle hamburger menu toggle
 function handleHamburger() {
   console.log("handleHamburger function called");
   const hamburger = document.querySelector(".hamburger");
   const listElmsContain = document.querySelector(".list-elms-contain");
 
+  //showing the hamburger menu with Toggle 'active' class on hamburger and navigation menu
   hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     listElmsContain.classList.toggle("active");
   });
 
+  // Closing the navigation menu when any menu item is clicked
   document.querySelectorAll(".list-elms").forEach((list) =>
     list.addEventListener("click", () => {
       hamburger.classList.remove("active");
@@ -16,32 +19,37 @@ function handleHamburger() {
   );
 }
 
+// URL for fetching news data
 const url = "https://saurav.tech/NewsAPI/top-headlines/category/health/in.json";
 
+// Function to fetch and display initial news details
 function getDetails() {
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
       if (data.articles) {
+        // Displaying first 4 articles in the News section of the page
         const newsCategories = document.getElementById("news-categories");
-
         data.articles.slice(0, 4).forEach((article) => {
+          // Creating HTML elements for each article
           const articleContainer = document.createElement("div");
           articleContainer.id = "article-container";
-
           const titleElm = document.createElement("h3");
           const imageElm = document.createElement("img");
           const descriptionElm = document.createElement("p");
 
+          // giving content to HTML elements
           titleElm.textContent = article.title;
           imageElm.src = article.urlToImage;
           imageElm.alt = article.title;
           descriptionElm.textContent = article.description;
 
+          // Appending the elements to the container
           articleContainer.appendChild(titleElm);
           articleContainer.appendChild(imageElm);
           articleContainer.appendChild(descriptionElm);
 
+          // Appending container to the News section
           newsCategories.appendChild(articleContainer);
         });
       } else {
@@ -51,31 +59,35 @@ function getDetails() {
     .catch((error) => console.error("Error fetching data:", error));
 }
 
+// Function to fetch and display additional news details
 function getAdditionalDetails() {
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
       if (data.articles) {
+        // Displaying additional articles in the Hot Topics section
         const additionalNewsSection =
           document.getElementById("additional-news");
-
         data.articles.slice(4, 10).forEach((article) => {
+          // Creating HTML elements for each additional article
           const topicsContainer = document.createElement("div");
           topicsContainer.className = "additional-news-container";
-
           const topicsTitle = document.createElement("h3");
           const topicsImage = document.createElement("img");
           const topicsDescription = document.createElement("p");
 
+          //Giving content to HTML elements
           topicsTitle.textContent = article.title;
           topicsImage.src = article.urlToImage;
           topicsImage.alt = article.title;
           topicsDescription.textContent = article.description;
 
+          // Appending the elements to the container  to display on the page
           topicsContainer.appendChild(topicsTitle);
           topicsContainer.appendChild(topicsImage);
           topicsContainer.appendChild(topicsDescription);
 
+          // Appending the container to the Hot Topics section to display on the page
           additionalNewsSection.appendChild(topicsContainer);
         });
       } else {
@@ -85,20 +97,24 @@ function getAdditionalDetails() {
     .catch((error) => console.error("Error fetching additional data:", error));
 }
 
+// Function to implement a simple slideshow of news articles
 function slideShow() {
   const articleContainer = document.getElementById("articleContainer");
   const prevButtonContainer = document.getElementById("prevButtonContainer");
   const nextButtonContainer = document.getElementById("nextButtonContainer");
   let currentIndex = 0;
 
+  // Function to update content in the slideshow
   function updateContent(data) {
     const firstArticle = data.articles[currentIndex];
     const secondArticle =
       data.articles[(currentIndex + 1) % data.articles.length];
 
+    // Creating HTML containers for the two articles in the slideshow
     const firstContainer = document.createElement("div");
     const secondContainer = document.createElement("div");
 
+    // GIving content to the containers
     firstContainer.innerHTML = `
         <div class="cardy">
           <h3 class="cardy-title">${firstArticle.title}</h3>
@@ -115,17 +131,20 @@ function slideShow() {
         </div>
       `;
 
+    // Clearing existing any content and appending new content
     articleContainer.innerHTML = "";
     articleContainer.appendChild(firstContainer);
     articleContainer.appendChild(secondContainer);
   }
 
+  // Fetching data and initializing the slideshow
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
       if (data.articles && data.articles.length > 0) {
         updateContent(data);
 
+        // Event listeners for navigating the slideshow
         prevButtonContainer.addEventListener("click", () => {
           currentIndex =
             (currentIndex - 1 + data.articles.length) % data.articles.length;
@@ -142,7 +161,9 @@ function slideShow() {
     });
 }
 
+// Function to handle smooth scrolling for navigation links
 function navbarScroll() {
+  //binding the section id's to their respective nav class names
   const homeLink = document.querySelector(".home-link");
   homeLink.addEventListener("click", scroller.bind(null, "news-reports"));
 
@@ -159,6 +180,7 @@ function navbarScroll() {
   subscribeLink.addEventListener("click", scroller.bind(null, "subscribe"));
 }
 
+// Function to scroll to a specific section on the page
 function scroller(sectionId) {
   const section = document.getElementById(sectionId);
   if (section) {
@@ -166,6 +188,7 @@ function scroller(sectionId) {
   }
 }
 
+// Function to post user subscriptions to db.json file
 function subscribers() {
   const subscribeButton = document.querySelector(".subscribe-button");
 
